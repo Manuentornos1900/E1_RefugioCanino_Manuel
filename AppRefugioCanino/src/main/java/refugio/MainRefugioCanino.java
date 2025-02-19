50192d8d612b51e5e6374d11e94b7d836e6b60eb
ManuEntornos1900 <mmediavillaa02@educantabria.es>
2025-02-19 19:41:47 +0100

    Refugio Canino Perro añadido1

--- AppRefugioCanino/src/main/java/refugio/MainRefugioCanino.java
+++ AppRefugioCanino/src/main/java/refugio/MainRefugioCanino.java
@@ -13,7 +13,7 @@ public class MainRefugioCanino {
         // Agregar algunos perros al refugio
         refugio.agregarPerro("Max", "Labrador");
         refugio.agregarPerro("Bella", "Pastor Alemán");
-         refugio.agregarPerro("Bea", "Pastor Al");
+         refugio.agregarPerro("Be", "Pastor Al");
 
         // Mostrar la lista de perros en el refugio
         System.out.println("\nLista de perros en el refugio:");
